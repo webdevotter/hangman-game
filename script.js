bf54b63f8f143f7dde25c534d96c8eb51562f5e3
Larry Rhodes <81366018+webdevotter@users.noreply.github.com>
2023-05-09 20:43:43 -0500

    Letter Press Event Handler

--- script.js
+++ script.js
@@ -36,4 +36,42 @@ ${selectedWord
   }
 }
 
+//Update the wrong letters
+function updateWrongLettersEl() {
+  console.log('Update wrong');
+}
+
+//Show notification
+function showNotification() {
+  notification.classList.add('show');
+
+  setTimeout(() => {
+    notification.classList.remove('show');
+  }, 2000);
+}
+
+//Keydown letter press
+window.addEventListener('keydown', (e) => {
+  if (e.keyCode >= 65 && e.keyCode <= 90) {
+    const letter = e.key;
+    if (selectedWord.includes(letter)) {
+      if (!correctLetters.includes(letter)) {
+        correctLetters.push(letter);
+
+        displayWord();
+      } else {
+        showNotification();
+      }
+    } else {
+      if (!wrongLetters.includes(letter)) {
+        wrongLetters.push(letter);
+
+        updateWrongLettersEl();
+      } else {
+        showNotification();
+      }
+    }
+  }
+});
+
 displayWord();
