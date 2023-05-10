0a1e227e6ee788f0b728988bd07fd0bf4ba689a6
Larry Rhodes <81366018+webdevotter@users.noreply.github.com>
2023-05-09 21:03:24 -0500

    Wrong Letters & Play Again

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const playAgainBtn = document.getElementById('play-button');
 const popup = document.getElementById('popup-container');
 const notification = document.getElementById('notification-container');
 const finalMessage = document.getElementById('final-message');
+const finalMessageRevealWord = document.getElementById(
+  'final-message-reveal-word'
+);
 
 const figureParts = document.querySelectorAll('.figure-part');
 
@@ -11,37 +14,67 @@ const words = ['application', 'programming', 'interface', 'wizard'];
 
 let selectedWord = words[Math.floor(Math.random() * words.length)];
 
-console.log(selectedWord);
+let playable = true;
 
 const correctLetters = [];
 const wrongLetters = [];
 
-//Show hidden word
+// Show hidden word
 function displayWord() {
   wordEl.innerHTML = `
-${selectedWord
-  .split('')
-  .map(
-    (letter) => `
-  <span class="letter">
-  ${correctLetters.includes(letter) ? letter : ''}</span>`
-  )
-  .join('')}
-`;
+    ${selectedWord
+      .split('')
+      .map(
+        (letter) => `
+          <span class="letter">
+            ${correctLetters.includes(letter) ? letter : ''}
+          </span>
+        `
+      )
+      .join('')}
+  `;
 
-  const innerWord = wordEl.innerText.replace(/\n/g, '');
+  const innerWord = wordEl.innerText.replace(/[ \n]/g, '');
+
   if (innerWord === selectedWord) {
     finalMessage.innerText = 'Congratulations! You won! 😃';
+    finalMessageRevealWord.innerText = '';
     popup.style.display = 'flex';
+
+    playable = false;
   }
 }
 
-//Update the wrong letters
+// Update the wrong letters
 function updateWrongLettersEl() {
-  console.log('Update wrong');
+  // Display wrong letters
+  wrongLettersEl.innerHTML = `
+    ${wrongLetters.length > 0 ? '<p>Wrong</p>' : ''}
+    ${wrongLetters.map((letter) => `<span>${letter}</span>`)}
+  `;
+
+  // Display parts
+  figureParts.forEach((part, index) => {
+    const errors = wrongLetters.length;
+
+    if (index < errors) {
+      part.style.display = 'block';
+    } else {
+      part.style.display = 'none';
+    }
+  });
+
+  // Check if lost
+  if (wrongLetters.length === figureParts.length) {
+    finalMessage.innerText = 'Unfortunately you lost. 😕';
+    finalMessageRevealWord.innerText = `...the word was: ${selectedWord}`;
+    popup.style.display = 'flex';
+
+    playable = false;
+  }
 }
 
-//Show notification
+// Show notification
 function showNotification() {
   notification.classList.add('show');
 
@@ -50,28 +83,48 @@ function showNotification() {
   }, 2000);
 }
 
-//Keydown letter press
+// Keydown letter press
 window.addEventListener('keydown', (e) => {
-  if (e.keyCode >= 65 && e.keyCode <= 90) {
-    const letter = e.key;
-    if (selectedWord.includes(letter)) {
-      if (!correctLetters.includes(letter)) {
-        correctLetters.push(letter);
+  if (playable) {
+    if (e.keyCode >= 65 && e.keyCode <= 90) {
+      const letter = e.key.toLowerCase();
 
-        displayWord();
-      } else {
-        showNotification();
-      }
-    } else {
-      if (!wrongLetters.includes(letter)) {
-        wrongLetters.push(letter);
+      if (selectedWord.includes(letter)) {
+        if (!correctLetters.includes(letter)) {
+          correctLetters.push(letter);
 
-        updateWrongLettersEl();
+          displayWord();
+        } else {
+          showNotification();
+        }
       } else {
-        showNotification();
+        if (!wrongLetters.includes(letter)) {
+          wrongLetters.push(letter);
+
+          updateWrongLettersEl();
+        } else {
+          showNotification();
+        }
       }
     }
   }
+});
+
+// Restart game and play again
+playAgainBtn.addEventListener('click', () => {
+  playable = true;
+
+  //  Empty arrays
+  correctLetters.splice(0);
+  wrongLetters.splice(0);
+
+  selectedWord = words[Math.floor(Math.random() * words.length)];
+
+  displayWord();
+
+  updateWrongLettersEl();
+
+  popup.style.display = 'none';
 });
 
 displayWord();
